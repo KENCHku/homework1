d19275c1c9b1cc2f4eae6747148411cb7e72dbf0
KENCHku <kench.mkk3@gmail.com>
2020-09-22 21:19:45 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        String whyHeWasLate = "108";
+
+        int num = 8;
+
+        String word = whyHeWasLate + num;
+
+        System.out.println(" dog" + 101);
+
+
+        int temperature = 26;
+
+        if (num < 0) {
+            System.out.println("вы сохранили отрицательное число");
+        }else if (num > 0) {
+            System.out.println("вы сохранили положительное число");
+
+        } else {
+            System.out.println("вы сохранили нуль");
+        }
+
+        System.out.println(word + " " + whyHeWasLate + " " + num);
+
+
+        num = 9;
+
+        System.out.println(num);
+
+
+        String name = "Kench";
+        System.out.println("hello "  + name);
+
+
+    }
+
+}
+
+
+
+
+
+
+
